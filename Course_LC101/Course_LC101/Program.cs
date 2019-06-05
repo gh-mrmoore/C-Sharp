8872791b5687a6be17d6170a6078b23c3f3308e2
mrm <matt.rmoore@gmail.com>
2019-06-05 12:44:18 -0500

    First commit of Unit 1 prep work

---
+++ Course_LC101/Course_LC101/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Course_LC101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
