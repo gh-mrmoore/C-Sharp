3aa48695770795e3a8b4ac90f0af6a9ae6a765d9
mrm <matt.rmoore@gmail.com>
2019-08-15 16:57:58 -0500

    Unit 4 basic prep complete - no delete functionality as yet

---
+++ Course_LC101/Unit04_Studio/Unit04_Studio/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Unit04_Studio.Models;
+
+namespace Unit04_Studio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static Dictionary<string, string> Dogs = new Dictionary<string, string>();
+
+        public IActionResult Index()
+        {
+            ViewBag.dogs = Dogs;
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Home/Add")]
+        public IActionResult ProcessAdd(string dogbreed, string dogsize)
+        {
+            Dogs.Add(dogbreed, dogsize);
+
+            return Redirect("/Home");
+        }
+
+        [HttpPost]
+        [Route("/Home/DeleteMany")]
+        public IActionResult DeleteMany(string[] dogs)
+        {
+            foreach (string dog in dogs)
+            {
+                Console.WriteLine(dog);
+            }
+
+            return Redirect("/Home");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Course_LC101/Unit04_Studio/Unit04_Studio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Unit04_Studio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
