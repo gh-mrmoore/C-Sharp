67e19970d3cc4e06e1240d5466c64c44c2e9cbdf
mrm <matt.rmoore@gmail.com>
2020-11-24 19:47:32 -0600

    testing adding a single class

---
+++ Course_LC101v2/C09_Exceptions_VS_Tools/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static double Divide(double x, double y)
+        {
+            if(y == 0)
+            {
+                throw new ArgumentOutOfRangeException("Don't divide by zero!");
+            } else
+            {
+                return x / y;
+            }
+        }
+
+        static int CheckFileExtension(string fileName)
+        {
+            // is there a blank file name or a null value passed for file name?
+            // if there is a file name and it doesn't end in .cs, 0 points
+            // if there is a file name and it does end in .cs, 1 point
+            if(fileName == "" || fileName == null)
+            {
+                   throw new ArgumentNullException("No file name recorded or submitted.");
+            }
+            else if(fileName.IndexOf(".cs") > 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Test out your Divide() function here!
+            try
+            {
+                Console.WriteLine(Divide(1, 0));
+            }
+            catch (ArgumentOutOfRangeException zeroError)
+            {
+                Console.WriteLine(zeroError.Message);
+            }
+
+            try
+            {
+                Console.WriteLine(Divide(2, 4));
+            }
+            catch (ArgumentOutOfRangeException wrongDivide)
+            {
+                Console.WriteLine(wrongDivide.Message);
+            }
+
+
+            // Test out your CheckFileExtension() function here!
+            Dictionary<string, string> students = new Dictionary<string, string>();
+            students.Add("Carl", "Program.xls");
+            students.Add("Brad", "");
+            students.Add("Elizabeth", "MyCode.cs");
+            students.Add("Stefanie", "CoolProgram.cs");
+
+            foreach(KeyValuePair<string, string> studentFile in students)
+            {
+                try
+                {
+                    Console.WriteLine("Grade for " + studentFile.Key + " is " + CheckFileExtension(studentFile.Value));
+                }
+                catch (ArgumentNullException nullError)
+                {
+                    Console.WriteLine(nullError.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Course_LC101v2/C09_Exceptions_VS_Tools/Exceptions/Exceptions/Temperature.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions
+{
+    public class Temperature
+    {
+
+        private double fahrenheit;
+        public static double AbsoluteZeroFahrenheit { get; } = -459.67;
+
+        public Temperature(double fahrenheit)
+        {
+            try
+            {
+                Fahrenheit = fahrenheit;
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Fahrenheit = -459.67;
+            }
+            finally
+            {
+                Console.WriteLine("Fahrenheit cannot be less than -459.67.");
+            }
+        }
+
+        public double Fahrenheit
+        {
+            get
+            {
+                return fahrenheit;
+            }
+            set
+            {
+
+                if (value < AbsoluteZeroFahrenheit)
+                {
+                    throw new ArgumentOutOfRangeException("Value is below absolute zero");
+                }
+
+                fahrenheit = value;
+            }
+        }
+    }
+}
